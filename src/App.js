e34b245964ced9256b147533713f98b863ca9f01
Scott Bass <fatalcubez@gmail.com>
2018-06-01 09:47:06 -0400

    Started working on accept note modal window

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import HomeFriends from './HomeFriends';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Bank from './Bank/Bank';
-import store from './UserStore'
+import store from './UserStore';
+import NoteModal from './NoteModal';
+
+import { acceptNote } from './Utils';
 
 const App = () => (
   <Switch>
-    <Route exact path='/' component={Directory}/>
+    <Route exact path='/' component={Test}/>
     <Route path='/login' component={SULI}/>
     <Route path='/dashboard' component={Dashboard}/>
     <Route path='/sendnote' component={NoteToggle}/>
@@ -20,10 +23,18 @@ const App = () => (
   </Switch>
 )
 
+const Test = () => (
+  <div>
+    <NoteModal header="Accept Note" note={{sender:"edopp", amount:"58", message:"Here ya go."}}/>
+  </div>
+)
+
 let friends = ['Scott', 'Eric'];
 for(let i = 0; i < 100; i++) {
   friends.push(i);
 }
 export {friends};
+
+acceptNote('u1.0');
 
 export default App;
