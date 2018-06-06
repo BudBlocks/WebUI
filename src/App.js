0081786e1740268763dadff569161c3e3466c31d
Scott Bass <fatalcubez@gmail.com>
2018-06-06 08:42:46 -0400

    Removed app directory and made it so the default landing page is the login screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Directory from './Directory';
 import SULI from './Login/SULI';
 import Dashboard from './Dashboard/Dashboard';
 import NoteToggle from './SendNote/NoteToggle';
@@ -10,19 +9,12 @@ import Bank from './Bank/Bank';
 
 const App = () => (
   <Switch>
-    <Route exact path='/' component={Directory}/>
-    <Route path='/login' component={SULI}/>
+    <Route exact path='/' component={SULI}/>
     <Route path='/dashboard' component={Dashboard}/>
     <Route path='/sendnote' component={NoteToggle}/>
     <Route path='/friends' component={HomeFriends}/>
     <Route path='/bank' component={Bank}/>
   </Switch>
 )
-
-let friends = ['Scott', 'Eric'];
-for(let i = 0; i < 100; i++) {
-  friends.push(i);
-}
-export {friends};
 
 export default App;
